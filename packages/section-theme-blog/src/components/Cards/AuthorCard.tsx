85408b3a9f8a2221372abc5b32ac36f4adf4eacc
officialrajdeepsingh <radewala.2@gmail.com>
2023-05-26 23:57:56 +0530

    update the author UI card

--- packages/section-theme-blog/src/components/Cards/AuthorCard.tsx
+++ packages/section-theme-blog/src/components/Cards/AuthorCard.tsx
@@ -1,35 +1,70 @@
-import {
-  Group,
-  Avatar,
-  Text,
-  createStyles,
-  ActionIcon,
-  Box,
-} from "@mantine/core";
+import { createStyles, Text, Title, Image, rem } from "@mantine/core";
+import { ActionIcon } from "@mantine/core";
+import { MdxFileAuthorCard } from "../../../types";
 import { IconBrandTwitter, IconBrandGithub } from "@tabler/icons-react";
 import Link from "next/link";
-import { MdxFileAuthorCard } from "../../../types";
 
 const useStyles = createStyles((theme) => ({
-  center: {
-    textAlign: "center",
-  },
-  link: {
-    textDecoration: "none",
-    color: theme.colorScheme === "dark" ? theme.white : theme.black,
-  },
-  box: {
-    alignItems: "center",
+  wrapper: {
     display: "flex",
-    flexDirection: "column",
-    padding: "10px",
+    alignItems: "center",
+    padding: `calc(${theme.spacing.xl} * 2)`,
+    borderRadius: theme.radius.md,
+    backgroundColor:
+      theme.colorScheme === "dark" ? theme.colors.dark[8] : theme.white,
+    border: `${rem(1)} solid ${
+      theme.colorScheme === "dark" ? theme.colors.dark[8] : theme.colors.gray[3]
+    }`,
 
-    "&:hover": {
-      backgroundColor:
-        theme.colorScheme === "dark"
-          ? theme.colors.dark[5]
-          : theme.colors.gray[1],
+    [theme.fn.smallerThan("sm")]: {
+      flexDirection: "column",
+      padding: theme.spacing.xl,
     },
+  },
+
+  image: {
+    maxWidth: "40%",
+
+    [theme.fn.smallerThan("sm")]: {
+      maxWidth: "100%",
+    },
+  },
+
+  body: {
+    paddingLeft: `calc(${theme.spacing.xl} * 4)`,
+
+    [theme.fn.smallerThan("sm")]: {
+      paddingLeft: 0,
+      marginTop: theme.spacing.xl,
+    },
+  },
+
+  title: {
+    color: theme.colorScheme === "dark" ? theme.white : theme.black,
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+    lineHeight: 1,
+    marginBottom: theme.spacing.md,
+  },
+
+  controls: {
+    display: "flex",
+    marginTop: theme.spacing.xl,
+  },
+
+  inputWrapper: {
+    width: "100%",
+    flex: "1",
+  },
+
+  input: {
+    borderTopRightRadius: 0,
+    borderBottomRightRadius: 0,
+    borderRight: 0,
+  },
+
+  control: {
+    borderTopLeftRadius: 0,
+    borderBottomLeftRadius: 0,
   },
 }));
 
@@ -39,50 +74,53 @@ export function AuthorCard({ subItem }: { subItem: MdxFileAuthorCard }) {
   if (subItem.frontMatter === undefined) {
     throw new Error("frontMatter is missing");
   }
+
   return (
-    <Box className={classes.box}>
-      <Avatar src={subItem?.frontMatter?.image} radius="xl" />
+    <div className={classes.wrapper}>
+      {subItem?.frontMatter?.image !== undefined ? (
+        <Image
+          src={
+            typeof subItem?.frontMatter?.image === "string"
+              ? subItem?.frontMatter?.image
+              : subItem?.frontMatter?.image[0].url
+          }
+          alt={subItem?.frontMatter.title}
+          className={classes.image}
+        />
+      ) : (
+        ""
+      )}
 
-      <div style={{ flex: 1 }}>
-        <Link href={subItem?.route} className={classes.link}>
-          <Text
-            className={classes.center}
-            component="h1"
-            size="sm"
-            weight={500}
-          >
-            {subItem?.frontMatter.name}
-          </Text>
-        </Link>
+      <div className={classes.body}>
+        <Title className={classes.title}>{subItem.frontMatter.name}</Title>
 
-        <Text
-          className={classes.center}
-          component="h2"
-          color="dimmed"
-          size="xs"
-        >
-          {subItem.frontMatter.description}
+        <Text fw={500} fz="lg" mb={5}>
+          {subItem.frontMatter.job}
         </Text>
-      </div>
 
-      <Group>
-        {subItem?.frontMatter.social &&
-          subItem?.frontMatter?.social.map(
-            (item: { name: string; url: string }) => {
-              return (
-                <Link key={item.name} target="_blank" href={item.url}>
-                  <ActionIcon size="lg">
-                    {item.name === "github" ? (
-                      <IconBrandGithub size={"1.1rem"} stroke={"1.5"} />
-                    ) : (
-                      <IconBrandTwitter size={"1.1rem"} stroke={"1.5"} />
-                    )}
-                  </ActionIcon>
-                </Link>
-              );
-            }
-          )}
-      </Group>
-    </Box>
+        <Text fz="sm" c="dimmed">
+          {subItem.frontMatter.except}
+        </Text>
+
+        <div className={classes.controls}>
+          {subItem?.frontMatter.social &&
+            subItem?.frontMatter?.social.map(
+              (item: { name: string; url: string }) => {
+                return (
+                  <Link key={item.name} target="_blank" href={item.url}>
+                    <ActionIcon size="lg">
+                      {item.name === "github" ? (
+                        <IconBrandGithub size={"1.1rem"} stroke={"1.5"} />
+                      ) : (
+                        <IconBrandTwitter size={"1.1rem"} stroke={"1.5"} />
+                      )}
+                    </ActionIcon>
+                  </Link>
+                );
+              }
+            )}
+        </div>
+      </div>
+    </div>
   );
 }
