ae7cc7920ccd8c078222643b7a783a8f820d1489
officialrajdeepsingh <radewala.2@gmail.com>
2023-05-30 16:10:37 +0530

    seo support

--- packages/section-theme-blog/src/components/Layouts/Posts.tsx
+++ packages/section-theme-blog/src/components/Layouts/Posts.tsx
@@ -7,19 +7,36 @@ import {
   TypographyStylesProvider,
 } from "@mantine/core";
 import { useRouter } from "next/router";
-import type { PageOpts, PageMapItem } from "nextra";
+import type { PageOpts, PageMapItem, ThemeConfig } from "nextra";
 import { MdxFileCard } from "../../../types";
+import { NextSeo } from "next-seo";
+import { getMetaImage } from "@/utlis/meta-images";
 
 export function Posts({
   children,
   pageOpts,
+  themeConfig,
 }: {
   children: React.ReactNode;
   pageOpts?: PageOpts;
+  themeConfig?: ThemeConfig;
 }) {
+  const { siteURL } = themeConfig;
   const router = useRouter();
+
   return (
     <>
+      <NextSeo
+        title={pageOpts?.frontMatter.title}
+        description={pageOpts?.frontMatter.except}
+        canonical={`${siteURL}${pageOpts?.route}`}
+        openGraph={{
+          url: pageOpts?.route,
+          title: pageOpts?.frontMatter.name,
+          description: pageOpts?.frontMatter.except,
+          images: getMetaImage(pageOpts?.frontMatter.image),
+        }}
+      />
       <Box my={"md"} maw={724} mx="auto">
         <TypographyStylesProvider> {children}</TypographyStylesProvider>
       </Box>
