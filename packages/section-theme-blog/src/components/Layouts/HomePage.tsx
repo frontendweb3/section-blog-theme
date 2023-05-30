bfb3dddf4c455f7bb962e6f0c6fefa68cdf80179
officialrajdeepsingh <radewala.2@gmail.com>
2023-05-30 16:09:22 +0530

    adding SEO support

--- packages/section-theme-blog/src/components/Layouts/HomePage.tsx
+++ packages/section-theme-blog/src/components/Layouts/HomePage.tsx
@@ -1,10 +1,36 @@
 import React from "react";
 import { TypographyStylesProvider, Box } from "@mantine/core";
+import { NextSeo } from "next-seo";
+import type { PageOpts, ThemeConfig } from "nextra";
+import { getMetaImage } from "@/utlis/meta-images";
 
-export const HomePage = ({ children }: { children: React.ReactNode }) => {
+export const HomePage = ({
+  children,
+  pageOpts,
+  themeConfig,
+}: {
+  children: React.ReactNode;
+  pageOpts?: PageOpts;
+  themeConfig?: ThemeConfig;
+}) => {
+  const { siteURL } = themeConfig;
+
   return (
-    <Box maw={724} mx="auto">
-      <TypographyStylesProvider> {children}</TypographyStylesProvider>
-    </Box>
+    <>
+      <NextSeo
+        title={pageOpts?.frontMatter.title}
+        description={pageOpts?.frontMatter.except}
+        canonical={siteURL}
+        openGraph={{
+          url: pageOpts?.route,
+          title: pageOpts?.frontMatter.name,
+          description: pageOpts?.frontMatter.except,
+          images: getMetaImage(pageOpts?.frontMatter.image),
+        }}
+      />
+      <Box maw={724} mx="auto">
+        <TypographyStylesProvider> {children}</TypographyStylesProvider>
+      </Box>
+    </>
   );
 };
