b4670117cde8ca3d9a3678957c87ea7e8ef37d52
officialrajdeepsingh <radewala.2@gmail.com>
2023-05-30 16:10:03 +0530

    adding SEO support

--- packages/section-theme-blog/src/components/Layouts/Page.tsx
+++ packages/section-theme-blog/src/components/Layouts/Page.tsx
@@ -1,22 +1,47 @@
 import React from "react";
 import { TypographyStylesProvider, Box, Title, Text } from "@mantine/core";
-import type { PageOpts } from "nextra";
+import type { PageOpts, ThemeConfig } from "nextra";
 import dayjs from "dayjs";
+import { NextSeo } from "next-seo";
+import { getMetaImage } from "@/utlis/meta-images";
 
-export const Page = ({ children, pageOpts }: { children: React.ReactNode; pageOpts?: PageOpts; }) => {
+export const Page = ({
+  children,
+  pageOpts,
+  themeConfig,
+}: {
+  children: React.ReactNode;
+  pageOpts?: PageOpts;
+  themeConfig?: ThemeConfig;
+}) => {
+  const { siteURL } = themeConfig;
   return (
-    <Box maw={724} mx="auto">
-      <Box py={"lg"} mt={"lg"}>
-        <Title order={1}>{pageOpts?.frontMatter.title}</Title>
-        {pageOpts?.frontMatter.date ? (
-          <Text>
-            {dayjs(pageOpts?.frontMatter.date).format("MMM DD, YYYY")}
-          </Text>
-        ) : (
-          ""
-        )}
+    <>
+      <NextSeo
+        title={pageOpts?.frontMatter.title}
+        description={pageOpts?.frontMatter.except}
+        canonical={`${siteURL}${pageOpts?.route}`}
+        openGraph={{
+          url: pageOpts?.route,
+          title: pageOpts?.frontMatter.name,
+          description: pageOpts?.frontMatter.except,
+          images: getMetaImage(pageOpts?.frontMatter.image),
+        }}
+      />
+
+      <Box maw={724} mx="auto">
+        <Box py={"lg"} mt={"lg"}>
+          <Title order={1}>{pageOpts?.frontMatter.title}</Title>
+          {pageOpts?.frontMatter.date ? (
+            <Text>
+              {dayjs(pageOpts?.frontMatter.date).format("MMM DD, YYYY")}
+            </Text>
+          ) : (
+            ""
+          )}
+        </Box>
+        <TypographyStylesProvider> {children}</TypographyStylesProvider>
       </Box>
-      <TypographyStylesProvider> {children}</TypographyStylesProvider>
-    </Box>
+    </>
   );
 };
