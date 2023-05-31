f3437b7c73ec9fb8b3659037df8409c538937090
officialrajdeepsingh <radewala.2@gmail.com>
2023-05-31 19:52:59 +0530

    hero component

---
+++ apps/docs/src/components/Hero.tsx
@@ -0,0 +1,144 @@
+import {
+  createStyles,
+  Title,
+  Text,
+  Button,
+  Container,
+  rem,
+} from "@mantine/core";
+import { GithubIcon } from "@mantine/ds";
+
+const useStyles = createStyles((theme) => ({
+  wrapper: {
+    position: "relative",
+    paddingTop: rem(120),
+    paddingBottom: rem(80),
+
+    [theme.fn.smallerThan("sm")]: {
+      paddingTop: rem(80),
+      paddingBottom: rem(60),
+    },
+  },
+
+  inner: {
+    position: "relative",
+    zIndex: 1,
+  },
+
+  dots: {
+    position: "absolute",
+    color:
+      theme.colorScheme === "dark"
+        ? theme.colors.dark[5]
+        : theme.colors.gray[1],
+
+    [theme.fn.smallerThan("sm")]: {
+      display: "none",
+    },
+  },
+
+  dotsLeft: {
+    left: 0,
+    top: 0,
+  },
+
+  title: {
+    textAlign: "center",
+    fontWeight: 800,
+    fontSize: rem(40),
+    letterSpacing: -1,
+    color: theme.colorScheme === "dark" ? theme.white : theme.black,
+    marginBottom: theme.spacing.xs,
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+
+    [theme.fn.smallerThan("xs")]: {
+      fontSize: rem(28),
+      textAlign: "left",
+    },
+  },
+
+  highlight: {
+    color:
+      theme.colors[theme.primaryColor][theme.colorScheme === "dark" ? 4 : 6],
+  },
+
+  description: {
+    textAlign: "center",
+
+    [theme.fn.smallerThan("xs")]: {
+      textAlign: "left",
+      fontSize: theme.fontSizes.md,
+    },
+  },
+
+  controls: {
+    marginTop: theme.spacing.lg,
+    display: "flex",
+    justifyContent: "center",
+
+    [theme.fn.smallerThan("xs")]: {
+      flexDirection: "column",
+    },
+  },
+
+  control: {
+    marginLeft: "10px",
+
+    [theme.fn.smallerThan("xs")]: {
+      height: rem(42),
+      fontSize: theme.fontSizes.md,
+
+      "&:not(:first-of-type)": {
+        marginTop: "10px",
+        marginLeft: 0,
+      },
+    },
+  },
+}));
+
+export function Hero() {
+  const { classes } = useStyles();
+
+  return (
+    <Container className={classes.wrapper} size={1400}>
+      <div className={classes.inner}>
+        <Title className={classes.title}>
+          Installation of the{" "}
+          <Text component="span" className={classes.highlight} inherit>
+            section theme blog
+          </Text>{" "}
+          with nextra
+        </Title>
+
+        <Container p={0} size={600}>
+          <Text size="lg" color="dimmed" className={classes.description}>
+            Build more reliable software with AI companion. AI is also trained
+            to detect lazy developers who do nothing and just complain on
+            Twitter.
+          </Text>
+        </Container>
+
+        <div className={classes.controls}>
+          <Button
+            className={classes.control}
+            size="xl"
+            variant="default"
+            color="gray"
+          >
+            Installation
+          </Button>
+          <Button
+            component="a"
+            href="https://github.com/mantinedev/mantine"
+            size="xl"
+            variant="default"
+            className={classes.control}
+            leftIcon={<GithubIcon size={20} />}
+          >
+            GitHub
+          </Button>
+        </div>
+      </div>
+    </Container>
+  );
+}
