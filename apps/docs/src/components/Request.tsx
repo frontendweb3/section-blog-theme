7bcccf0a37c0ff45fb5855640227950967a39f2b
officialrajdeepsingh <radewala.2@gmail.com>
2023-06-01 12:49:35 +0530

    fix the button css

---
+++ apps/docs/src/components/Request.tsx
@@ -0,0 +1,49 @@
+import { Container, Button, Text, rem, createStyles } from "@mantine/core";
+import Link from "next/link";
+const useStyles = createStyles((theme) => ({
+  control: {
+    marginLeft: "10px",
+    "&: hover": {
+      color:
+        theme.colorScheme === "dark"
+          ? theme.colors.dark[0]
+          : theme.colors.gray[2],
+      backgroundColor:
+        theme.colorScheme === "dark"
+          ? theme.colors.gray[0]
+          : theme.colors.dark[2],
+    },
+  },
+}));
+export function Request() {
+  const { classes } = useStyles();
+
+  return (
+    <Container
+      sx={(theme) => ({
+        display: "flex",
+        alignItems: "center",
+        margin: "30px auto",
+        background: theme.colors.dark[5],
+        justifyContent: "space-evenly",
+        paddingTop: "10px",
+        paddingBottom: "10px",
+      })}
+    >
+      <Text size="lg" color="dimmed">
+        Submit new issues and feature requests on the section theme blog.
+      </Text>
+
+      <Button
+        component="a"
+        size="xl"
+        variant="default"
+        color="blue"
+        className={classes.control}
+        href="https://github.com/mantinedev/mantine"
+      >
+        GitHub
+      </Button>
+    </Container>
+  );
+}
