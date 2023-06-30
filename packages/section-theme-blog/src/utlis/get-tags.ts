0b03aee73a494da5fe77026c27c2acc940a30a01
officialrajdeepsingh <radewala.2@gmail.com>
2023-06-30 23:11:04 +0530

    delete the unused get-tags file

--- packages/section-theme-blog/src/utlis/get-tags.ts
+++
@@ -1,9 +0,0 @@
-import type { MdxFile } from "nextra";
-import type { BlogFrontMatter } from "../types";
-
-export function getStaticTags(page: MdxFile<BlogFrontMatter>) {
-  if (!page.frontMatter) {
-    return [];
-  }
-  return page.frontMatter.tags;
-}
