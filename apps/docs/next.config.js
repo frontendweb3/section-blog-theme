377a67b4016fce0b584e3dd61aeef26fd0abf0f2
officialrajdeepsingh <radewala.2@gmail.com>
2023-08-18 17:22:14 +0530

    change the folder app to examples

--- apps/docs/next.config.js
+++
@@ -1,9 +0,0 @@
-const withNextra = require("nextra")({
-  theme: "nextra-theme-docs",
-  themeConfig: "./theme.config.tsx",
-  staticImage: true,
-  defaultShowCopyCode: true,
-  readingTime: true,
-});
-
-module.exports = withNextra();
