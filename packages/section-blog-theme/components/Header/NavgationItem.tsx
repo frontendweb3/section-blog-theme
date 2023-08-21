88aaaa62ddecf96a1b972fca55e8320074450337
officialrajdeepsingh <radewala.2@gmail.com>
2023-08-21 18:46:16 +0530

    Navgation for Header

---
+++ packages/section-blog-theme/components/Header/NavgationItem.tsx
@@ -0,0 +1,24 @@
+import {
+  NavigationMenuItem,
+  NavigationMenuLink,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import { Navigation } from "@/src/types";
+import Link from "next/link";
+
+
+const NavigationItem = ({ navigation }: { navigation: Navigation }) => {
+
+    return (
+      <NavigationMenuItem className="my-2 md:my-0">
+        <Link href={navigation.href as string} legacyBehavior passHref>
+          <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+            {navigation.title}
+          </NavigationMenuLink>
+        </Link>
+      </NavigationMenuItem>
+    );
+  
+};
+
+export default NavigationItem;
