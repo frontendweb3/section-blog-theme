1d473b9a3f1d4e7d2765f18c392d42d3696c9cb2
officialrajdeepsingh <radewala.2@gmail.com>
2023-08-22 22:28:40 +0530

    update the tailwind config

--- packages/section-blog-theme/tailwind.config.js
+++ packages/section-blog-theme/tailwind.config.js
@@ -1,4 +1,8 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
+
 module.exports = {
   darkMode: ["class"],
   content: [
@@ -69,8 +73,8 @@ module.exports = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-      },
+      } 
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"), require('@tailwindcss/typography'), require('@tailwindcss/container-queries')],
 }
