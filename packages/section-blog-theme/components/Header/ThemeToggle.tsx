1db223667abfc8f173ea9bc3d7dd3ef42f6f1b0c
officialrajdeepsingh <radewala.2@gmail.com>
2023-08-22 20:21:45 +0530

    update the Theme toggle

--- packages/section-blog-theme/components/Header/ThemeToggle.tsx
+++ packages/section-blog-theme/components/Header/ThemeToggle.tsx
@@ -1,16 +1,10 @@
-import * as React from "react"
 import { MoonIcon, SunIcon } from "@radix-ui/react-icons"
 import { useTheme } from "next-themes"
-
 import { Button } from "@/components/ui/button"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
 
 export function ThemeToggle() {
+
   const { setTheme } = useTheme()
 
   return (
