cb140c1d3afd8139001df71a039f4181afed9795
officialrajdeepsingh <radewala.2@gmail.com>
2023-08-27 17:20:52 +0530

    button component

--- packages/section-blog-theme/components/ui/button.tsx
+++ packages/section-blog-theme/components/ui/button.tsx
@@ -1,8 +1,7 @@
 import * as React from "react"
 import { Slot } from "@radix-ui/react-slot"
 import { cva, type VariantProps } from "class-variance-authority"
-
-import { cn } from "@/utility"
+import { cn } from "@/utility/utils"
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
