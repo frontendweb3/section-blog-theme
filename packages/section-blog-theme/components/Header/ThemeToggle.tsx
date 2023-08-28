4e48712c4af15bdf57c10ad10d8f325946e272a9
officialrajdeepsingh <radewala.2@gmail.com>
2023-08-28 17:42:51 +0530

    update the button icon

--- packages/section-blog-theme/components/Header/ThemeToggle.tsx
+++ packages/section-blog-theme/components/Header/ThemeToggle.tsx
@@ -10,9 +10,9 @@ export function ThemeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <Button variant="secondary" className="ml-1" size="icon">
+          <SunIcon className="h-5 w-5  rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <MoonIcon className=" h-5 w-5 absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
