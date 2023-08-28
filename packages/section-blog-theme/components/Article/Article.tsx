d3b72639c1c17430531fed565373f67e8beab04c
officialrajdeepsingh <radewala.2@gmail.com>
2023-08-28 17:41:01 +0530

    update the Article component

--- packages/section-blog-theme/components/Article/Article.tsx
+++ packages/section-blog-theme/components/Article/Article.tsx
@@ -8,6 +8,6 @@ interface ArticleProps {
 
 export function Article({className, children}:ArticleProps) {
   return (
-       <article className={ cn("py-3 px-3 my-16 sm:px-0 prose prose-zinc prose-sm md:prose-base lg:prose-lg xl:prose-xl 2xl:prose-2xl dark:prose-invert mx-auto",className)}> { children } </article> 
+       <article className={ cn("container py-3 px-3 my-16 sm:px-0 prose prose-zinc prose-sm md:prose-base lg:prose-lg xl:prose-xl 2xl:prose-2xl dark:prose-invert mx-auto",className)}> { children } </article> 
   );
 }
