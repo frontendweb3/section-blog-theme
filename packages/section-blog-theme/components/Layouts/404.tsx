8ecccddae129d81e9a9602d4ddb63fd7ab71a6dc
officialrajdeepsingh <radewala.2@gmail.com>
2023-08-31 17:51:28 +0530

    404 Error

--- packages/section-blog-theme/components/Layouts/404.tsx
+++ packages/section-blog-theme/components/Layouts/404.tsx
@@ -1,25 +1,29 @@
 import Link from "next/link";
-export function NotFound() {
+export function Error404() {
   return (
-    <section className="flex items-center h-full p-16 dark:text-gray-100">
+    <section className="flex items-center h-full p-16 ">
       <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
         <div className="max-w-md text-center">
-          <h2 className="mb-8 font-extrabold text-9xl dark:text-gray-600">
+          
+          <h2 className="mb-8 font-extrabold text-9xl ">
             <span className="sr-only">Error</span>404
           </h2>
+          
           <p className="text-2xl font-semibold md:text-3xl">
             Sorry, we couldn't find this page.
           </p>
-          <p className="mt-4 mb-8 dark:text-gray-400">
+          
+          <p className="mt-4 mb-8 ">
             But dont worry, you can find plenty of other things on our homepage.
           </p>
+          
           <Link
-            rel="noopener noreferrer"
-            href="#"
+            href="/"
             className="px-8 py-3 font-semibold rounded dark:bg-violet-400 dark:text-gray-900"
           >
             Back to homepage
           </Link>
+        
         </div>
       </div>
     </section>
