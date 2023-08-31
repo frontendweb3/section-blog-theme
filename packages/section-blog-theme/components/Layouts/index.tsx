f3105a3abe505344ea460a6b9820e69e3adfde56
officialrajdeepsingh <radewala.2@gmail.com>
2023-08-31 17:54:04 +0530

    Import and Export layouts

---
+++ packages/section-blog-theme/components/Layouts/index.tsx
@@ -0,0 +1,8 @@
+import { Page } from "@/components/Layouts/Page"
+import { Error404 } from "@/components/Layouts/404"
+import { Error500 } from "@/components/Layouts/500"
+import { Posts } from "@/components/Layouts/Posts"
+import { Read } from "@/components/Layouts/Read"
+import { HomePage } from "@/components/Layouts/HomePage"
+
+export { Page, Error404, Error500, Posts, HomePage, Read}
