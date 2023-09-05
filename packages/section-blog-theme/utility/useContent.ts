7601a8ca519fc6a3271c5451c0b8be73b67b743e
officialrajdeepsingh <radewala.2@gmail.com>
2023-09-05 13:42:38 +0530

    useContent hook

--- packages/section-blog-theme/utility/useContent.ts
+++ packages/section-blog-theme/utility/useContent.ts
@@ -2,15 +2,27 @@ import type {  PageOpts } from "nextra";
 import type { MdxFileCard } from "@/src/types"
 
 export function useContent({ pageMap, frontMatter }: PageOpts) {
-  
+
+  // posts
   let tempPosts: MdxFileCard[] | undefined = [];
 
   for (let page of pageMap) {
-    if (page.kind === "Folder") {
+    if (page.kind === "Folder" && page.name==="posts") {
       tempPosts?.push(...page?.children);
     }
   }
-  let posts = tempPosts.filter((temp) => temp?.name !== "index");
+  let posts = tempPosts.filter((temp:MdxFileCard) => temp?.name !== "index" && temp?.kind !== "Meta");
 
-  return [posts, frontMatter];
+  // Authors
+    let tempAuthors: MdxFileCard[] | undefined = [];
+
+  for (let page of pageMap) {
+    if (page.kind === "Folder" && page.name==="authors") {      
+      tempAuthors?.push(...page?.children);
+    }
+  }
+
+  let authors = tempAuthors.filter((temp) => temp?.name !== "index" && temp?.kind !== "Meta");  
+  return { posts: posts, authors:authors, frontMatter:frontMatter };
+
 }
