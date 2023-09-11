60f372f090d6c3b14891980bc0e5f55417f9724f
officialrajdeepsingh <radewala.2@gmail.com>
2023-09-11 15:57:50 +0530

    adding SEO

--- packages/section-blog-theme/components/Layouts/500.tsx
+++ packages/section-blog-theme/components/Layouts/500.tsx
@@ -1,32 +1,35 @@
+import { NextSeo } from "next-seo";
 import Link from "next/link";
 
 export function Error500() {
   return (
-    <section className="flex items-center h-full p-16 ">
-      <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
-        <div className="max-w-md text-center">
-          
-          <h2 className="mb-8 font-extrabold text-9xl ">
-            <span className="sr-only">Error</span>500
-          </h2>
-          
-          <p className="text-2xl font-semibold md:text-3xl">
-            Sorry, we couldn't find this page.
-          </p>
-          
-          <p className="mt-4 mb-8 ">
-            But dont worry, you can find plenty of other things on our homepage.
-          </p>
-          
-          <Link
-            href="#"
-            className="px-8 py-3 font-semibold rounded dark:bg-violet-400 dark:text-gray-900"
-          >
-            Back to homepage
-          </Link>
-        
+    <>
+      <NextSeo noindex={true} />
+      <section className="flex items-center h-full p-16 ">
+        <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
+          <div className="max-w-md text-center">
+            <h2 className="mb-8 font-extrabold text-9xl ">
+              <span className="sr-only">Error</span>500
+            </h2>
+
+            <p className="text-2xl font-semibold md:text-3xl">
+              Sorry, we couldn't find this page.
+            </p>
+
+            <p className="mt-4 mb-8 ">
+              But dont worry, you can find plenty of other things on our
+              homepage.
+            </p>
+
+            <Link
+              href="#"
+              className="px-8 py-3 font-semibold rounded dark:bg-violet-400 dark:text-gray-900"
+            >
+              Back to homepage
+            </Link>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
