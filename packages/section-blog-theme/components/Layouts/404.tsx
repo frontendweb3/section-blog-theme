c29ea4ac0b3fb0c2d753764431783b817a936f17
officialrajdeepsingh <radewala.2@gmail.com>
2023-09-11 15:57:42 +0530

    Adding SEO support

--- packages/section-blog-theme/components/Layouts/404.tsx
+++ packages/section-blog-theme/components/Layouts/404.tsx
@@ -1,7 +1,10 @@
+import { NextSeo } from "next-seo";
 import Link from "next/link";
 
 export function Error404() {
   return (
+    <>
+    <NextSeo noindex={true} />
     <section className="flex items-center h-full p-16 ">
       <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
         <div className="max-w-md text-center">
@@ -28,5 +31,6 @@ export function Error404() {
         </div>
       </div>
     </section>
+    </>
   );
 }
