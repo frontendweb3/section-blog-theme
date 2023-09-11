0d9cce5434189794e53a63182fcd6c1302df3ee7
officialrajdeepsingh <radewala.2@gmail.com>
2023-09-11 15:58:43 +0530

    SEO component

---
+++ packages/section-blog-theme/components/Seo/Seo.tsx
@@ -0,0 +1,37 @@
+import { NextSeo } from "next-seo";
+import { AuthorFrontMatter, BlogFrontMatter } from "@/src/types";
+import { FrontMatter } from "nextra"
+
+export function Seo({ frontMatter }: { frontMatter: BlogFrontMatter }) {
+
+  console.log(frontMatter)
+
+  return (
+    <NextSeo
+      title={frontMatter.title}
+      description={frontMatter.description}
+      canonical="https://www.canonical.ie/"
+      openGraph={{
+        url: "https://www.url.ie/a",
+        title: frontMatter.title,
+        description: frontMatter.description,
+        images: [
+          {
+            url: "https://www.example.ie/og-image-01.jpg",
+            width: 800,
+            height: 600,
+            alt: "Og Image Alt",
+            type: "image/jpeg",
+          },
+          { url: "https://www.example.ie/og-image-02.jpg" }
+        ],
+        siteName: "SiteName",
+      }}
+      twitter={{
+        handle: "@handle",
+        site: "@site",
+        cardType: "summary_large_image",
+      }}
+    />
+  );
+}
