b6319c769ab546ebcc572e2719ecb745b3f8c3cb
officialrajdeepsingh <radewala.2@gmail.com>
2023-09-11 16:20:14 +0530

    adding SEO support

--- packages/section-blog-theme/components/Layouts/Posts.tsx
+++ packages/section-blog-theme/components/Layouts/Posts.tsx
@@ -5,33 +5,27 @@ import type { PageOpts, ThemeConfig } from "nextra";
 import { useContent } from "@/utility/useContent";
 import dayjs from "dayjs";
 
-export function Posts(
-  { children, themeConfig, pageOpts }: {
-    pageOpts: PageOpts;
-    themeConfig: ThemeConfig;
-    children: React.ReactNode;
-  },
-) {
-  const [posts, frontMatter] = useContent(pageOpts);
+export function Posts({ children, themeConfig, pageOpts }: { pageOpts: PageOpts; themeConfig: ThemeConfig; children: React.ReactNode;}) {
+  
+  const { posts } = useContent(pageOpts);
+  
   return (
     <>
       <Article>{children}</Article>
       {posts?.map(
         (post) => {
-          return post.kind !== "Meta"
-            ? (
-              <ArticleCard
-                key={post.frontMatter.date + post.frontMatter.title}
-                title={post.frontMatter.title}
-                description={post.frontMatter.description}
-                tag={post.frontMatter.tags}
-                date={dayjs(post.frontMatter.date).format("DD MMM YYYY")}
-                URL={post.route}
-                authorName={post.frontMatter.author}
-                authorURL="/"
-              />
-            )
-            : null;
+          return (
+            <ArticleCard
+              key={post.frontMatter.date + post.frontMatter.title}
+              title={post.frontMatter.title}
+              description={post.frontMatter.description}
+              tag={post.frontMatter.tags}
+              date={dayjs(post.frontMatter.date).format("DD MMM YYYY")}
+              URL={post.route}
+              authorName={post.frontMatter?.author || undefined}
+              authorURL="/"
+            />
+          );
         },
       )}
     </>
