06bd57c6ed755755113bca04e6bccd19eeaa5721
officialrajdeepsingh <radewala.2@gmail.com>
2023-09-11 16:07:39 +0530

    Author List

---
+++ examples/demo-section-blog/src/components/AuthorList.tsx
@@ -0,0 +1,15 @@
+export function AuthorList() {
+  return (
+    <div className="mx-auto text-center">
+      
+      <h2 className="mb-4 text-4xl tracking-tight font-extrabold">
+        List of Authors
+      </h2>
+      
+      <p className="font-light">
+        Explore the whole collection of open-source web components and elements
+        built with the utility classes from Tailwind
+      </p>
+    </div>
+  );
+}
