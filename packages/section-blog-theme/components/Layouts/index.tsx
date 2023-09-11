91afbc358c977c5866ac522a0957a866623b397e
officialrajdeepsingh <radewala.2@gmail.com>
2023-09-11 16:18:28 +0530

    export the Authors and Author component

--- packages/section-blog-theme/components/Layouts/index.tsx
+++ packages/section-blog-theme/components/Layouts/index.tsx
@@ -4,5 +4,8 @@ import { Error500 } from "@/components/Layouts/500"
 import { Posts } from "@/components/Layouts/Posts"
 import { Read } from "@/components/Layouts/Read"
 import { HomePage } from "@/components/Layouts/HomePage"
+import { Authors } from "@/components/Layouts/Authors"
+import { Author } from "@/components/Layouts/Author"
 
-export { Page, Error404, Error500, Posts, HomePage, Read}
+
+export { Page, Error404, Error500, Posts, HomePage, Read, Authors, Author}
