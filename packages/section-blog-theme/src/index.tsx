35be6ffd0add1b806bb22d52db81e97a033b4bc9
Rajdeep Singh <radewala.2@gmail.com>
2023-10-14 15:14:16 +0530

    rewrite it

--- packages/section-blog-theme/src/index.tsx
+++ packages/section-blog-theme/src/index.tsx
@@ -3,18 +3,31 @@ import { Header } from "@/components/Header/Header";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { Footer } from "@/components/Footer/Footer"
 import { BlogLayout } from "@/components/Layouts/BlogLayout"
+import { Banner } from "@/components/banner/banner";
+import { useBannerCookies } from "@/utility/useCookies";
+import { useEffect, useState } from "react";
 
 export default function Layout(props: NextraThemeLayoutProps) {
+  const { banner, hideBanner } = useBannerCookies();
+  const [showBanner, setBanner] = useState<string>("hide")
+  const { children, themeConfig, pageOpts } = props;
+  const { Logo, PrimaryNavigation, SecondaryNavigation, SocialLinks, bannerMessage } = themeConfig;
 
-  const { children, themeConfig,pageOpts } = props;
+  useEffect(function() {
+    setBanner(banner)
+  }, [banner])
 
-  const { Logo, PrimaryNavigation, SecondaryNavigation, SocialLinks } = themeConfig
- 
   return (
     <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
+
+      {showBanner === "show" && bannerMessage !== undefined ? <Banner hideBanner={hideBanner} message={bannerMessage} /> : ""}
+
       <Header socialLinks={SocialLinks} Logo={Logo} PrimaryNavigation={PrimaryNavigation} />
+
       <BlogLayout pageOpts={pageOpts} themeConfig={themeConfig}>{children}</BlogLayout>
+
       <Footer socialLinks={SocialLinks} Logo={Logo} SecondaryNavigation={SecondaryNavigation} />
+
     </NextThemesProvider>
   );
 }
