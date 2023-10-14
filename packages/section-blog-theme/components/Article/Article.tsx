d4b4109b56c54299441d9ca40d8b7ab9e993716e
Rajdeep Singh <radewala.2@gmail.com>
2023-10-14 15:06:26 +0530

    fix the code background color issue and adding margin top and bottom

--- packages/section-blog-theme/components/Article/Article.tsx
+++ packages/section-blog-theme/components/Article/Article.tsx
@@ -6,8 +6,8 @@ interface ArticleProps {
   className?: object | string;
 }
 
-export function Article({className, children}:ArticleProps) {
+export function Article({ className, children }: ArticleProps) {
   return (
-       <article className={ cn("container py-3 px-3 my-16 sm:px-0 prose prose-pre:bg-primary-foreground prose-zinc prose-sm md:prose-base lg:prose-lg xl:prose-xl 2xl:prose-2xl dark:prose-invert mx-auto",className)}> { children } </article> 
+    <article className={cn("container py-3 px-3 my-5 sm:px-0 prose prose-pre:bg-slate-700/25 dark:prose-pre:bg-primary-foreground prose-zinc sm:prose-sm md:prose-base lg:prose-lg xl:prose-xl 2xl:prose-2xl dark:prose-invert mx-auto", className)}> {children} </article>
   );
 }
