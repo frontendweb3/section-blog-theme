eb16ae2b5a292b855544a0a0ca4530cea9f2c634
Rajdeep Singh <radewala.2@gmail.com>
2023-10-14 15:13:20 +0530

    remove the author, authors import and export, export the tag layout

--- packages/section-blog-theme/components/Layouts/index.tsx
+++ packages/section-blog-theme/components/Layouts/index.tsx
@@ -1,11 +1,8 @@
-import { Page } from "@/components/Layouts/Page"
-import { Error404 } from "@/components/Layouts/404"
-import { Error500 } from "@/components/Layouts/500"
-import { Posts } from "@/components/Layouts/Posts"
-import { Read } from "@/components/Layouts/Read"
-import { HomePage } from "@/components/Layouts/HomePage"
-import { Authors } from "@/components/Layouts/Authors"
-import { Author } from "@/components/Layouts/Author"
-
-
-export { Page, Error404, Error500, Posts, HomePage, Read, Authors, Author}
+import { Page } from "@/components/Layouts/Page";
+import { Error404 } from "@/components/Layouts/404";
+import { Error500 } from "@/components/Layouts/500";
+import { Posts } from "@/components/Layouts/Posts";
+import { Read } from "@/components/Layouts/Read";
+import { HomePage } from "@/components/Layouts/HomePage";
+import { Tag } from "@/components/Layouts/Tag";
+export { Page, Error404, Error500, Posts, HomePage, Read, Tag }
