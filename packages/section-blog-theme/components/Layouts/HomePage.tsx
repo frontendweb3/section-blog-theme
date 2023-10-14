38c7c8d0e99b30daab23aed14b2aaec1bc582176
Rajdeep Singh <radewala.2@gmail.com>
2023-10-14 15:09:47 +0530

    fix the indentation

--- packages/section-blog-theme/components/Layouts/HomePage.tsx
+++ packages/section-blog-theme/components/Layouts/HomePage.tsx
@@ -1,13 +1,9 @@
 import { Article } from "@/components/Article/Article";
 import * as React from "react";
 import { Seo } from "@/components/Seo/Seo";
-import { PageOpts, ThemeConfig } from "nextra";
+import type { PageOpts, ThemeConfig } from "nextra";
 import { useContent } from "@/utility/useContent";
-export function HomePage({ pageOpts, themeConfig, children }: {
-  pageOpts: PageOpts;
-  themeConfig: ThemeConfig;
-  children: React.ReactNode;
-}) {
+export function HomePage({ pageOpts, themeConfig, children }: { pageOpts: PageOpts; themeConfig: ThemeConfig; children: React.ReactNode; }) {
 
   const { frontMatter } = useContent(pageOpts);
 
