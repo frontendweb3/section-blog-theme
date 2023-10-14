8e175477b0d1dd8aeb294aa6f63d433cad8da05c
Rajdeep Singh <radewala.2@gmail.com>
2023-10-14 15:10:31 +0530

    rewrite the posts UI

--- packages/section-blog-theme/components/Layouts/Posts.tsx
+++ packages/section-blog-theme/components/Layouts/Posts.tsx
@@ -4,30 +4,39 @@ import * as React from "react";
 import type { PageOpts, ThemeConfig } from "nextra";
 import { useContent } from "@/utility/useContent";
 import dayjs from "dayjs";
+import { Seo } from "../Seo/Seo";
 
-export function Posts({ children, themeConfig, pageOpts }: { pageOpts: PageOpts; themeConfig: ThemeConfig; children: React.ReactNode;}) {
-  
-  const { posts } = useContent(pageOpts);
-  
+export function Posts({ children, themeConfig, pageOpts }: { pageOpts: PageOpts; themeConfig: ThemeConfig; children: React.ReactNode; }) {
+
+  const { posts, frontMatter } = useContent(pageOpts);
+
+  const { DateFormat } = themeConfig
+
   return (
     <>
+
+      <Seo frontMatter={frontMatter} />
+
       <Article>{children}</Article>
-      {posts?.map(
-        (post) => {
-          return (
-            <ArticleCard
-              key={post.frontMatter.date + post.frontMatter.title}
-              title={post.frontMatter.title}
-              description={post.frontMatter.description}
-              tag={post.frontMatter.tags}
-              date={dayjs(post.frontMatter.date).format("DD MMM YYYY")}
-              URL={post.route}
-              authorName={post.frontMatter?.author || undefined}
-              authorURL="/"
-            />
-          );
-        },
-      )}
+
+      <div className="mx-auto my-24 divide-y divide-slate-700 grid-cols-1 grid max-w-[724px] gap-4">
+        {posts?.map(
+          (post) => {
+            return (
+              <ArticleCard
+                key={post.frontMatter.date + post.frontMatter.title}
+                title={post.frontMatter.title}
+                description={post.frontMatter.description}
+                tag={post.frontMatter.tags}
+                date={dayjs(post.frontMatter.date).format(DateFormat)}
+                URL={post.route}
+                author={post.frontMatter?.author}
+              />
+            );
+          },
+        )}
+      </div>
+
     </>
   );
 }
