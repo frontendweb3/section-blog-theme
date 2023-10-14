146ccf4feece52287c3dcfd592dbb3fd929ec456
Rajdeep Singh <radewala.2@gmail.com>
2023-10-14 15:00:57 +0530

    bundler the tag

--- packages/section-blog-theme/tsup.config.ts
+++ packages/section-blog-theme/tsup.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from "tsup";
 import tsconfig from "./tsconfig.json";
 
 export default defineConfig({
-  entry: ["src/index.tsx","styles/globals.css"],
-  dts:{
-    entry: ["src/index.tsx"]
+  entry: ["src/index.tsx", "styles/globals.css", "src/tag.tsx"],
+  dts: {
+    entry: ["src/index.tsx", "src/tag.tsx"]
   },
-  format: ["esm","cjs"],
+  format: ["esm", "cjs"],
   name: "section-blog-theme",
   outExtension({ format }) {
     return {
