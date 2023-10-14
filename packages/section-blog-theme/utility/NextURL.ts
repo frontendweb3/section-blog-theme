41f7d6f7a5ba592525374ab88a5184582e474a2c
Rajdeep Singh <radewala.2@gmail.com>
2023-10-14 15:16:17 +0530

    rewrite it

--- packages/section-blog-theme/utility/NextURL.ts
+++ packages/section-blog-theme/utility/NextURL.ts
@@ -1,5 +1,3 @@
-// export const Next_URL :string =(URL:string)=> process.env.NODE_ENV === "production"?  process.env.vercel_url? process.env.vercel_url : URL : "http://localhost:3000"
-
 export function Next_URL(URL: string) {
   if (process.env.NODE_ENV === "production") {
     if (process.env.vercel_url) {
