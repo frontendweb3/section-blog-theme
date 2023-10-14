88d26d5205f086d6c672f131df7be25161ce2888
Rajdeep Singh <radewala.2@gmail.com>
2023-10-14 15:16:49 +0530

    fix the indentation

--- packages/section-blog-theme/utility/useContent.ts
+++ packages/section-blog-theme/utility/useContent.ts
@@ -1,4 +1,4 @@
-import type {  PageOpts } from "nextra";
+import type { PageOpts } from "nextra";
 import type { MdxFileCard } from "@/src/types"
 
 export function useContent({ pageMap, frontMatter }: PageOpts) {
@@ -7,23 +7,23 @@ export function useContent({ pageMap, frontMatter }: PageOpts) {
   let tempPosts: MdxFileCard[] | undefined = [];
 
   for (let page of pageMap) {
-    if (page.kind === "Folder" && page.name==="posts") {
+    if (page.kind === "Folder" && page.name === "posts") {
       tempPosts?.push(...page?.children);
     }
   }
 
-  let posts = tempPosts.filter((temp:MdxFileCard) => temp?.name !== "index" && temp?.kind !== "Meta");
+  let posts = tempPosts.filter((temp: MdxFileCard) => temp?.name !== "index" && temp?.kind !== "Meta");
 
   // Authors
-    let tempAuthors: MdxFileCard[] | undefined = [];
+  let tempAuthors: MdxFileCard[] | undefined = [];
 
   for (let page of pageMap) {
-    if (page.kind === "Folder" && page.name==="authors") {      
+    if (page.kind === "Folder" && page.name === "authors") {
       tempAuthors?.push(...page?.children);
     }
   }
 
-  let authors = tempAuthors.filter((temp) => temp?.name !== "index" && temp?.kind !== "Meta");  
-  
-  return { posts: posts, authors:authors, frontMatter:frontMatter };
+  let authors = tempAuthors.filter((temp) => temp?.name !== "index" && temp?.kind !== "Meta");
+
+  return { posts: posts, authors: authors, frontMatter: frontMatter };
 }
