b71807bb4d3e344ba3dc7235e9b96bd4268e21d7
Rajdeep Singh <radewala.2@gmail.com>
2023-10-14 15:08:12 +0530

    change the 404 and 500 error ui

--- packages/section-blog-theme/components/Layouts/500.tsx
+++ packages/section-blog-theme/components/Layouts/500.tsx
@@ -8,6 +8,7 @@ export function Error500() {
       <section className="flex items-center h-full p-16 ">
         <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
           <div className="max-w-md text-center">
+
             <h2 className="mb-8 font-extrabold text-9xl ">
               <span className="sr-only">Error</span>500
             </h2>
@@ -22,7 +23,7 @@ export function Error500() {
             </p>
 
             <Link
-              href="#"
+              href="/"
               className="px-8 py-3 font-semibold rounded dark:bg-violet-400 dark:text-gray-900"
             >
               Back to homepage
