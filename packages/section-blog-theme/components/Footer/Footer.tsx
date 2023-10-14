810bce6d6025e648dba13d758b51334708d5d536
Rajdeep Singh <radewala.2@gmail.com>
2023-10-14 15:06:59 +0530

    change the footer ui

--- packages/section-blog-theme/components/Footer/Footer.tsx
+++ packages/section-blog-theme/components/Footer/Footer.tsx
@@ -1,42 +1,39 @@
-import {
-  NavigationMenu,
-  NavigationMenuList,
-} from "@/components/ui/navigation-menu";
+import { NavigationMenu, NavigationMenuList } from "@/components/ui/navigation-menu";
 import type { Logo, Navigation, SocialLinks } from "@/src/types";
 import Link from "next/link";
 import NavigationItem from "@/components/Navigation/NavgationItem";
 import Image from "next/image";
 import { SocialLink } from "@/components/SocialLink/SocialLink";
 
-export function Footer(
-  { Logo, SecondaryNavigation, socialLinks }: {
-    Logo: Logo;
-    SecondaryNavigation: Navigation[]; socialLinks: SocialLinks[]
-  },
-) {
+export function Footer({ Logo, SecondaryNavigation, socialLinks }: { Logo: Logo; SecondaryNavigation: Navigation[]; socialLinks: SocialLinks[] }) {
+
   return (
-    <footer className="print:hidden container flex flex-wrap flex-row item-center justify-center my-5 p-5 mx-auto md:p-10 lg:flex-row divide-gray-400">
+    <footer className="print:hidden container flex flex-wrap flex-row item-center justify-center my-5 p-5 mx-auto lg:flex-row divide-gray-400">
+
       <Link href="/" className="flex justify-center items-center">
-        {Logo.ImageLightPath
-          ? (
-            <picture>
-              <source
-                srcSet={Logo.ImageDarkPath}
-                media="(prefers-color-scheme:dark)"
-              />
-              <source
-                srcSet={Logo.ImageLightPath}
-                media="(prefers-color-scheme:dark)"
-              />
-              <Image
-                width="34"
-                height="34"
-                src={Logo.ImageLightPath}
-                alt="logo"
-              />
-            </picture>
-          )
-          : ""}
+
+        {
+          Logo.ImageLightPath
+            ? (
+              <>
+                <Image
+                  className="block dark:hidden"
+                  width="34"
+                  height="34"
+                  src={Logo.ImageDarkPath}
+                  alt="logo"
+                />
+
+                <Image
+                  className="hidden dark:block"
+                  width="34"
+                  height="34"
+                  src={Logo.ImageLightPath}
+                  alt="logo"
+                />
+              </>
+            )
+            : ""}
         {Logo.TextLogo
           ? (
             <span className="ml-2 self-center text-xl font-semibold whitespace-nowrap !text-foreground dark:text-white">
@@ -44,22 +41,24 @@ export function Footer(
             </span>
           )
           : ""}
+
       </Link>
 
-      <div className="my-3 flex justify-between items-center mx-auto md:my-0" >
-        <NavigationMenu>
-          <NavigationMenuList className="flex-wrap">
-            {SecondaryNavigation.map((navigation: Navigation) => {
+
+      <NavigationMenu className="my-3 flex flex-wrap justify-between items-center mx-auto md:my-0">
+        <NavigationMenuList>
+          {
+            SecondaryNavigation.map((navigation: Navigation) => {
               return (
                 <NavigationItem
                   key={navigation.title}
                   navigation={navigation}
                 />
               );
-            })}
-          </NavigationMenuList>
-        </NavigationMenu>
-      </div>
+            })
+          }
+        </NavigationMenuList>
+      </NavigationMenu>
 
       <div className="flex flex-row justify-between gap-2 items-center">
 
