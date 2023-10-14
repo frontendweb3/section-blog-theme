32fed6c6d830e4b4e3b273cb1a908d0694facd8d
Rajdeep Singh <radewala.2@gmail.com>
2023-10-14 15:06:39 +0530

    change the card UI

--- packages/section-blog-theme/components/Card/Card.tsx
+++ packages/section-blog-theme/components/Card/Card.tsx
@@ -1,49 +1,29 @@
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import Link from "next/link";
-
-export function ArticleCard({ title, description, date, tag, URL, authorName, authorURL }: { title: string; description: string; date: string; tag: string[] | undefined; URL: string; authorName?: string; authorURL?: string; }) {
-  
+import type { authorType } from "@/src/types"
+export function ArticleCard({ title, description, date, tag, URL, author }: { title: string; description: string; date: string; tag: string[] | undefined; URL: string; author?: string | authorType; }) {
   return (
-    <Card className="my-16 p-8 container max-w-4xl px-10 py-6 mx-auto rounded-lg shadow-sm">
-      <CardHeader className="flex flex-row items-center justify-between">
-        <span className="text-xl">{date}</span>
-        {tag && tag.length > 0
-          ? (
-            <span className="text-xl px-2 py-1 font-bold rounded-md dark:bg-violet-400">
-              {tag[0]}
-            </span>
-          )
-          : ""}
-      </CardHeader>
-
-      <CardContent className="mt-3">
-        <CardTitle className="text-4xl font-bold">
-          {title}
-        </CardTitle>
-
-        <CardDescription className="text-2xl mt-2">
-          {description}
-        </CardDescription>
-      </CardContent>
-
-      <CardFooter className="flex flex-row items-center justify-between mt-4">
-        <Link
-          href={URL}
-          className="text-lg hover:underline dark:text-violet-400"
-        >
-          Read More
-        </Link>
-        {authorURL !== undefined && authorURL !== undefined
-          ? (
-            <Link href={authorURL}>
-              Publish by
-              <span className="ml-1 text-lg ">
-                {authorName}
-              </span>
-            </Link>
-          )
-          : ""}
-      </CardFooter>
-    </Card>
+    <Link href={URL}>
+      <div className="my-16 container  border-foreground/60 dark:border-foreground shadow-none flex-row flex max-[479px]:flex-col max-[479px]">
+        <div className="max-[479px]:max-w-xs px-0 sm:px-8 max-[479px]:pt-4">
+          <h1 className="text-2xl lg:text-4xl font-bold mb-1 lg:mb-1">{title}</h1>
+          <p className="text-lg lg:text-xl mb-2 md:mb-2 lg:mb-3">{description}</p>
+          <div className="mt-1 text-xs sm:text-sm">
+            {
+              typeof author === 'string' ? <span>Published By {author}</span> : typeof author === 'object' ? <span>Published By {author.name}</span> : ""} • {tag && tag.length > 0
+                ? (
+                  <span>
+                    {tag[0]}
+                  </span>
+                )
+                : ""} • <time
+                  dateTime={date}
+                  title={date}
+                >
+              {date}
+            </time>
+          </div>
+        </div>
+      </div>
+    </Link>
   );
 }
