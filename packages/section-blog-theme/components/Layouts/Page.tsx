543a12e92bfe30da679df7e49be5e0f590182594
Rajdeep Singh <radewala.2@gmail.com>
2023-10-14 15:10:00 +0530

    seo support

--- packages/section-blog-theme/components/Layouts/Page.tsx
+++ packages/section-blog-theme/components/Layouts/Page.tsx
@@ -1,6 +1,17 @@
 import { Article } from "@/components/Article/Article"
 import * as React from "react";
+import { Seo } from "../Seo/Seo";
+import { useContent } from "@/utility/useContent";
+import type { PageOpts } from "nextra";
 
-export function Page({children}:{ children : React.ReactNode}){
-return ( <Article> { children} </Article>)
+export function Page({ pageOpts, children }: { pageOpts: PageOpts; children: React.ReactNode; }) {
+
+  const { frontMatter } = useContent(pageOpts);
+
+  return (
+    <>
+      <Seo frontMatter={frontMatter} />
+      <Article> {children} </Article>
+    </>
+  )
 }
