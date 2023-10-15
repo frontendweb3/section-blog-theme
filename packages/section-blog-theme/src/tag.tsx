65219b6e9cdf435778e16a451369f54abf1b28c5
Rajdeep Singh <radewala.2@gmail.com>
2023-10-15 16:02:09 +0530

    change the slug to tag

--- packages/section-blog-theme/src/tag.tsx
+++ packages/section-blog-theme/src/tag.tsx
@@ -1,18 +1,35 @@
 import { useTags } from "@/utility/useTags";
 import type { GetStaticProps } from "next";
+// import { useData } from "nextra/data";
+import Head from 'next/head'
+import { useSSG } from 'nextra/ssg'
 
 const NEXTRA_INTERNAL = Symbol.for("__nextra_internal__");
+
+export const TagTitle = ({ prefix = 'Posts tagged with ' }: { prefix: string }) => {
+  const { tag } = useSSG()
+  const title = `${prefix}${tag}`
+  return (
+    <Head>
+      <title>{title}</title>
+    </Head>
+  )
+}
+
+export const TagName = () => {
+  const { tag } = useSSG()
+  return tag || null
+}
 
 export const getStaticProps: GetStaticProps = ({ params }) => {
   return {
     props: {
       ssg: {
-        slug: params?.slug,
-      },
-    },
-  };
-};
-
+        tag: params?.tag
+      }
+    }
+  }
+}
 export function getStaticPaths() {
 
   const globalData = (globalThis as any)[NEXTRA_INTERNAL].pageMap
