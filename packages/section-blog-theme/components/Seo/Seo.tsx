20f5e3a13959ba6c201345aa311127af9116052e
Rajdeep Singh <radewala.2@gmail.com>
2023-10-15 16:08:59 +0530

    remove the twiter for SEO

--- packages/section-blog-theme/components/Seo/Seo.tsx
+++ packages/section-blog-theme/components/Seo/Seo.tsx
@@ -1,35 +1,33 @@
 import { NextSeo } from "next-seo";
-import { BlogFrontMatter } from "@/src/types";
-import { FrontMatter } from "nextra";
+import { BlogFrontMatter, TypeSectionBlogTheme } from "@/src/types";
+import { Next_URL } from "@/utility/NextURL";
 
-export function Seo({ frontMatter }: { frontMatter: BlogFrontMatter }) {
+export function Seo({ frontMatter, themeConfig }: { frontMatter: BlogFrontMatter, themeConfig?: TypeSectionBlogTheme }) {
+
+  // const { settings } = themeConfig
 
   return (
-    <NextSeo
-      title={frontMatter.title}
-      description={frontMatter.description}
-      canonical="https://www.canonical.ie/"
-      openGraph={{
-        url: "https://www.url.ie/a",
-        title: frontMatter.title,
-        description: frontMatter.description,
-        images: [
-          {
-            url: "https://www.example.ie/og-image-01.jpg",
-            width: 800,
-            height: 600,
-            alt: "Og Image Alt",
-            type: "image/jpeg",
-          },
-          { url: "https://www.example.ie/og-image-02.jpg" }
-        ],
-        siteName: "SiteName",
-      }}
-      twitter={{
-        handle: "@handle",
-        site: "@site",
-        cardType: "summary_large_image",
-      }}
-    />
+    <>
+      <NextSeo
+        title={frontMatter.title}
+        description={frontMatter.description}
+        openGraph={{
+          url: "https://www.url.ie/a",
+          title: frontMatter.title,
+          description: frontMatter.description,
+          images: [
+            {
+              url: "https://www.example.ie/og-image-01.jpg",
+              width: 800,
+              height: 600,
+              alt: "Og Image Alt",
+              type: "image/jpeg",
+            },
+            { url: "https://www.example.ie/og-image-02.jpg" }
+          ],
+          siteName: "SiteName",
+        }}
+      />
+    </>
   );
 }
