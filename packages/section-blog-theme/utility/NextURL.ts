d123091a35157e8707c8adad677570093c94ea0c
Rajdeep Singh <radewala.2@gmail.com>
2023-10-15 15:52:10 +0530

    fix the indentation

--- packages/section-blog-theme/utility/NextURL.ts
+++ packages/section-blog-theme/utility/NextURL.ts
@@ -1,10 +1,13 @@
-export function Next_URL(URL: string) {
-  if (process.env.NODE_ENV === "production") {
+export function Next_URL(URL: string| undefined) {
+  
+  if (process.env.NODE_ENV === "production" && URL !== undefined) {
+    
     if (process.env.vercel_url) {
       return process.env.vercel_url;
     } else {
       return URL;
     }
+
   } else {
     return "http://localhost:3000";
   }
