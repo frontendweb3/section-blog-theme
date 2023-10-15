611646303fab7bb312a70dd10b8748e76dae07a4
Rajdeep Singh <radewala.2@gmail.com>
2023-10-15 15:51:42 +0530

    fix the indentation

--- packages/section-blog-theme/components/Article/Article.tsx
+++ packages/section-blog-theme/components/Article/Article.tsx
@@ -8,6 +8,6 @@ interface ArticleProps {
 
 export function Article({ className, children }: ArticleProps) {
   return (
-    <article className={cn("container py-3 px-3 my-5 sm:px-0 prose prose-pre:bg-slate-700/25 dark:prose-pre:bg-primary-foreground prose-zinc sm:prose-sm md:prose-base lg:prose-lg xl:prose-xl 2xl:prose-2xl dark:prose-invert mx-auto", className)}> {children} </article>
+    <article className={cn("container py-3 px-3 my-5 sm:px-0 prose dark:prose-p:text-white prose-pre:bg-slate-700/25 dark:prose-pre:bg-primary-foreground prose-zinc sm:prose-sm md:prose-base lg:prose-lg xl:prose-xl 2xl:prose-2xl dark:prose-invert mx-auto", className)}> {children} </article>
   );
 }
