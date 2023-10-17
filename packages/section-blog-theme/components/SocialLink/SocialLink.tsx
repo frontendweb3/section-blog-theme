f409c402f93f8c9b032e3f773050071725ed5f20
Rajdeep Singh <radewala.2@gmail.com>
2023-10-17 14:56:03 +0530

    Fix the path of Icon

--- packages/section-blog-theme/components/SocialLink/SocialLink.tsx
+++ packages/section-blog-theme/components/SocialLink/SocialLink.tsx
@@ -1,10 +1,9 @@
 import { Button } from "@/components/ui/button";
 import type { SocialLinks } from "@/src/types";
 import Link from "next/link";
-import Icon from "./DynmicIcon"; 
+import Icon from "@/components/SocialLink/DynmicIcon";
 
 export function SocialLink({ socialLink }: { socialLink: SocialLinks }) {
-
     return (
       <Link className={"self-center"} key={socialLink.name} target="_blank" href={socialLink.url}>
         <Button aria-label="Search article" variant={"ghost"} size={"icon"}>
