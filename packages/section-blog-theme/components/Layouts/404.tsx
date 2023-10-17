e8f4e2b25683e2cdee8f049e8f3c55e2d3c48d4b
Rajdeep Singh <radewala.2@gmail.com>
2023-10-17 11:36:39 +0530

    Adding title and description in NextSeo

--- packages/section-blog-theme/components/Layouts/404.tsx
+++ packages/section-blog-theme/components/Layouts/404.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export function Error404() {
   return (
     <>
-      <NextSeo noindex={true} />
+      <NextSeo title="404 Error" description="Something went wrong, sorry, we couldn't find this page." noindex={true} />
       <section className="flex items-center h-full p-16">
         <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
           <div className="max-w-md text-center">
