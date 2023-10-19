2bc0d6484354a745191ddeb51a4eaf49fabe0420
Rajdeep Singh <radewala.2@gmail.com>
2023-10-19 13:50:55 +0530

    define type for lucide-react

--- packages/section-blog-theme/components/SocialLink/DynmicIcon.tsx
+++ packages/section-blog-theme/components/SocialLink/DynmicIcon.tsx
@@ -1,15 +1,9 @@
 import dynamic from 'next/dynamic'
-import { LucideProps } from 'lucide-react';
 import dynamicIconImports from 'lucide-react/dynamicIconImports';
-
-interface IconProps extends LucideProps {
-  name: keyof typeof dynamicIconImports;
-}
+import type {IconProps} from "@/src/types";
 
 const Icon = ({ name, ...props }: IconProps) => {
   const LucideIcon = dynamic(dynamicIconImports[name])
-
   return <LucideIcon {...props} />;
 };
-
 export default Icon;
