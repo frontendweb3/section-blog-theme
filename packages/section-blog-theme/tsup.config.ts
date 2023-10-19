0243fd6f51eb228ad549bb290b0af057248eb9f8
Rajdeep Singh <radewala.2@gmail.com>
2023-10-19 15:42:54 +0530

    remove the cjs and esm

--- packages/section-blog-theme/tsup.config.ts
+++ packages/section-blog-theme/tsup.config.ts
@@ -1,18 +1,12 @@
 import { defineConfig } from "tsup";
-import tsconfig from "./tsconfig.json";
 
 export default defineConfig({
   entry: ["src/index.tsx", "styles/globals.css", "src/tag.tsx"],
   dts: {
     entry: ["src/index.tsx", "src/tag.tsx"]
   },
-  format: ["esm", "cjs"],
+  format: 'esm',
   name: "section-blog-theme",
-  outExtension({ format }) {
-    return {
-      js: `.${format}.js`,
-    }
-  },
-  target: tsconfig.compilerOptions.target as "es2016",
+  outExtension: () => ({ js: '.js' }),
   external: ["nextra"],
 });
