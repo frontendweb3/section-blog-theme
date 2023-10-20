13c3bb255fbc09f0a62621f1de1249889d378dfd
Rajdeep Singh <radewala.2@gmail.com>
2023-10-20 21:54:02 +0530

    fix the basis issue

--- packages/section-blog-theme/components/Seo/Seo.tsx
+++ packages/section-blog-theme/components/Seo/Seo.tsx
@@ -3,43 +3,47 @@ import { BlogFrontMatter, TypeSectionBlogTheme } from "@/src/types";
 import { Next_URL } from "@/utility/NextURL";
 
 export function Seo({ frontMatter, themeConfig }: { frontMatter: BlogFrontMatter, themeConfig?: TypeSectionBlogTheme }) {
-  const imageURL = typeof frontMatter.image === "string" ? frontMatter?.image : undefined
-  const keyword = frontMatter.tags !== undefined ? frontMatter?.tags : undefined
-  const author = typeof frontMatter.author === "string" ? frontMatter?.author : frontMatter.author
-  const twitterCardType = imageURL !== undefined ? {
-    cardType: "summary_large_image",
-  }
-    : {
-      cardType: "summary",
-    }
 
+  const imageURL = frontMatter.image && typeof frontMatter.image === "string" ? frontMatter?.image : frontMatter?.image as string
+  const keyword = typeof frontMatter.tags !== "object" ? frontMatter?.tags?.toString() : frontMatter?.tags
+  const author: string[] = typeof frontMatter.author === "string" ? [...frontMatter?.author] : frontMatter?.author
+  const twitterCardType = imageURL !== undefined ? { cardType: "summary_large_image" } : { cardType: "summary" }
+
+
+  if ((frontMatter.image && typeof frontMatter.image === "string" !== undefined) || (frontMatter.tags && typeof frontMatter.tags === "string" ? frontMatter?.tags : frontMatter?.tags)) {
+
+    const keyword = typeof frontMatter.tags !== "object" ? frontMatter?.tags?.toString() : frontMatter?.tags
+
+    return (<NextSeo
+      title={frontMatter.title}
+      description={frontMatter.description}
+      openGraph={
+        {
+          url: Next_URL(themeConfig?.settings?.SiteURL),
+          title: frontMatter.title,
+          description: frontMatter.description,
+          images: [
+            {
+              url: imageURL,
+            }
+          ],
+          keywords: keyword,
+          authors: author,
+          twitter: twitterCardType
+        }
+      }
+    />)
+  }
 
   return (
     <NextSeo
       title={frontMatter.title}
       description={frontMatter.description}
-      openGraph={
-
-        imageURL !== undefined || keyword !== undefined ?
-          {
-            url: Next_URL(themeConfig?.settings?.SiteURL),
-            title: frontMatter.title,
-            description: frontMatter.description,
-            images: [
-              {
-                url: imageURL,
-              }
-            ],
-            keywords: keyword,
-            authors: author,
-            twitter: twitterCardType
-          } : {
-            url: Next_URL(themeConfig?.settings?.SiteURL),
-            title: frontMatter.title,
-            description: frontMatter.description,
-          }
-
-      }
+      openGraph={{
+        url: Next_URL(themeConfig?.settings?.SiteURL),
+        title: frontMatter.title,
+        description: frontMatter.description,
+      }}
     />
   );
 }
