2edc3d110dab238f71ee8ae807410fae66b896eb
Rajdeep Singh <radewala.2@gmail.com>
2023-10-23 20:17:44 +0530

    remove the ErrorBoundary and  ThemeProvider

--- packages/section-blog-theme/src/index.tsx
+++ packages/section-blog-theme/src/index.tsx
@@ -1,36 +1,27 @@
 import type { PageOpts } from "nextra";
 import { Header } from "@/components/Header/Header";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { Footer } from "@/components/Footer/Footer"
 import { BlogLayout } from "@/components/Layouts/BlogLayout"
 import { Banner } from "@/components/banner/banner";
 import { ReactNode, useEffect, useState } from "react";
 import { DefaultSeo } from "next-seo";
 import { TypeSectionBlogTheme } from "./types";
-import { ErrorBoundary } from "react-error-boundary";
 import { useLocalStorage } from 'usehooks-ts'
+import { ThemeProvider } from "@/components/ThemeProvider/ThemeProvider";
 
-function fallbackRender({ error, resetErrorBoundary }) {
-  return (
-    <div role="alert">
-      <p>Something went wrong:</p>
-      <pre style={{ color: "red" }}>{error.message}</pre>
-    </div>
-  );
-}
 
 export default function Layout({ pageOpts, themeConfig, children }: {
   pageOpts: PageOpts; themeConfig: TypeSectionBlogTheme;
   children: ReactNode;
 }) {
 
-  // Hanndle banner 
+  // Handle banner
   const [banner, setBanner] = useLocalStorage('banner', true)
   const [showBanner, setShwobanner] = useState<boolean | undefined>()
   const toggleBanner = () => {
     setBanner((prevValue: boolean) => !prevValue)
   }
-  useEffect(function() {
+  useEffect(function () {
     setShwobanner(banner)
   }, [banner])
 
@@ -38,23 +29,17 @@ export default function Layout({ pageOpts, themeConfig, children }: {
   const { Logo, PrimaryNavigation, SecondaryNavigation, SocialLinks, bannerMessage, settings } = themeConfig;
 
   return (
+    <ThemeProvider>
+      {settings && settings?.defaultSEO && <DefaultSeo {...settings.defaultSEO} />}
 
-    <ErrorBoundary fallbackRender={fallbackRender} >
+      {showBanner && bannerMessage !== undefined ? <Banner hideBanner={toggleBanner} message={bannerMessage} /> : ""}
 
-      <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
-        {settings && settings?.defaultSEO && <DefaultSeo {...settings.defaultSEO} />}
+      <Header socialLinks={SocialLinks} Logo={Logo} PrimaryNavigation={PrimaryNavigation} />
 
-        {showBanner && bannerMessage !== undefined ? <Banner hideBanner={toggleBanner} message={bannerMessage} /> : ""}
+      <BlogLayout pageOpts={pageOpts} themeConfig={themeConfig}>{children}</BlogLayout>
 
-        <Header socialLinks={SocialLinks} Logo={Logo} PrimaryNavigation={PrimaryNavigation} />
-
-        <BlogLayout pageOpts={pageOpts} themeConfig={themeConfig}>{children}</BlogLayout>
-
-        <Footer socialLinks={SocialLinks} Logo={Logo} SecondaryNavigation={SecondaryNavigation} />
-
-      </NextThemesProvider>
-
-    </ErrorBoundary>
+      <Footer socialLinks={SocialLinks} Logo={Logo} SecondaryNavigation={SecondaryNavigation} />
+    </ThemeProvider>
   );
 }
 
