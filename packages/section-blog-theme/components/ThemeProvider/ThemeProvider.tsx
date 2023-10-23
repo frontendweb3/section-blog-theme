ca031caaf27562d2d316ae4ec97117bf370021a8
Rajdeep Singh <radewala.2@gmail.com>
2023-10-23 20:16:09 +0530

    ThemeProvider

---
+++ packages/section-blog-theme/components/ThemeProvider/ThemeProvider.tsx
@@ -0,0 +1,13 @@
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { ReactNode} from "react";
+
+export function ThemeProvider({children }: {children: ReactNode;}) {
+
+  
+  return (
+    <NextThemesProvider enableColorScheme={true} attribute="class" defaultTheme="system" enableSystem={true}>
+      {children}
+    </NextThemesProvider>
+  );
+}
+
