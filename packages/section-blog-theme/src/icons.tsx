66ef180ca33ec15db2ece56bc54b970d2cdf6feb
Rajdeep Singh <radewala.2@gmail.com>
2023-10-25 14:24:26 +0530

    Adding Alert, Tabs and Separator component

---
+++ packages/section-blog-theme/src/icons.tsx
@@ -0,0 +1,4 @@
+// import icons from lucide-react icon library > https://lucide.dev/icons
+import { AlertTriangle, Ban, Check, CheckCircle, HelpCircle, Terminal, Braces, BadgeAlert, BadgeX, Shield, Lightbulb } from 'lucide-react';
+// export 
+export { AlertTriangle, Ban, Check, CheckCircle, HelpCircle, Terminal, Braces, BadgeAlert, BadgeX, Shield, Lightbulb };
