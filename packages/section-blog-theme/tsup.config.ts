c8bfabd0e1bba030650afb67f4be5bd4a7a0c563
Rajdeep Singh <radewala.2@gmail.com>
2023-10-25 14:30:54 +0530

    Adding components and icons

--- packages/section-blog-theme/tsup.config.ts
+++ packages/section-blog-theme/tsup.config.ts
@@ -1,11 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-<<<<<<< Updated upstream
-  entry: ["src/index.tsx", "styles/globals.css", "src/tag.tsx"],
-=======
   entry: ["src/index.tsx", "styles/globals.css", "src/tag.tsx","src/components.tsx", "src/icons.tsx"],
->>>>>>> Stashed changes
   dts: {
     entry: ["src/index.tsx", "src/tag.tsx","src/components.tsx", "src/icons.tsx"]
   },
