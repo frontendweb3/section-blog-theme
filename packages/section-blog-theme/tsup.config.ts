5a2198a39d8e39b66907ea6925c58fca6ea572de
Rajdeep Singh <radewala.2@gmail.com>
2023-10-25 14:23:53 +0530

    Adding Alert, Tabs and Separator component

--- packages/section-blog-theme/tsup.config.ts
+++ packages/section-blog-theme/tsup.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
+<<<<<<< Updated upstream
   entry: ["src/index.tsx", "styles/globals.css", "src/tag.tsx"],
+=======
+  entry: ["src/index.tsx", "styles/globals.css", "src/tag.tsx","src/components.tsx", "src/icons.tsx"],
+>>>>>>> Stashed changes
   dts: {
-    entry: ["src/index.tsx", "src/tag.tsx"]
+    entry: ["src/index.tsx", "src/tag.tsx","src/components.tsx", "src/icons.tsx"]
   },
   format: 'esm',
   name: "section-blog-theme",
