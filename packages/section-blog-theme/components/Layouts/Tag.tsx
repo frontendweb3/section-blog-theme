9066e99b03bed63ae19bff257fd6bc0574f61613
Rajdeep Singh <radewala.2@gmail.com>
2023-10-29 15:20:42 +0530

    fix the #42

--- packages/section-blog-theme/components/Layouts/Tag.tsx
+++ packages/section-blog-theme/components/Layouts/Tag.tsx
@@ -48,13 +48,16 @@ export function Tag({ pageOpts, themeConfig, children }: { pageOpts: PageOpts; t
       <div className="mx-auto my-24 divide-y divide-slate-700 grid-cols-1 grid max-w-[724px] lg:max-w-[1024px] gap-4">
         {posts?.map(
           (post) => {
+            
+            let getDate  = dayjs(post.frontMatter.date).format(DateFormat? DateFormat : "MMM DD, YYYY");
+
             return (
               <ArticleCard
                 key={post.frontMatter.date + post.frontMatter.title}
                 title={post.frontMatter.title}
                 description={post.frontMatter.description}
                 tag={post.frontMatter.tags}
-                date={dayjs(post.frontMatter.date).format(DateFormat)}
+                date={getDate}
                 URL={post.route}
                 author={post.frontMatter?.author}
               />
