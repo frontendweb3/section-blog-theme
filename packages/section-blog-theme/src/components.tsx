7a7495e7fd8630aa0480d5582b187a3901d56fda
Rajdeep Singh <radewala.2@gmail.com>
2023-10-30 16:28:09 +0530

    Adding new #40

--- packages/section-blog-theme/src/components.tsx
+++ packages/section-blog-theme/src/components.tsx
@@ -1,3 +1,4 @@
 export { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 export { Separator } from "@/components/ui/separator";
 export { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+export { Space } from "@/components/Space/Space";
