b1d2ebd26be2921572fb6b0c2df5dd568ffe1589
Rajdeep Singh <radewala.2@gmail.com>
2023-11-01 16:03:39 +0530

    Now sort posts on Descending order

--- packages/section-blog-theme/utility/useContent.ts
+++ packages/section-blog-theme/utility/useContent.ts
@@ -1,5 +1,6 @@
 import type { PageOpts } from "nextra";
-import type { MdxFileCard } from "@/src/types"
+import type { MdxFileCard } from "@/src/types";
+import dayjs from "dayjs";
 
 export function useContent({ pageMap, frontMatter }: PageOpts) {
 
@@ -12,7 +13,15 @@ export function useContent({ pageMap, frontMatter }: PageOpts) {
     }
   }
 
+  // posts
   let posts = tempPosts.filter((temp: MdxFileCard) => temp?.name !== "index" && temp?.kind !== "Meta");
+
+   // Descending
+   let formatPostBasedOnDate = posts.sort(function (a, b) {
+    let dateA = dayjs(a.frontMatter.date)
+    let dateB = dayjs(b.frontMatter.date)
+    return dateB - dateA
+  });
 
   // Authors
   let tempAuthors: MdxFileCard[] | undefined = [];
@@ -25,5 +34,5 @@ export function useContent({ pageMap, frontMatter }: PageOpts) {
 
   let authors = tempAuthors.filter((temp) => temp?.name !== "index" && temp?.kind !== "Meta");
 
-  return { posts: posts, authors: authors, frontMatter: frontMatter };
+  return { posts: formatPostBasedOnDate, frontMatter: frontMatter };
 }
