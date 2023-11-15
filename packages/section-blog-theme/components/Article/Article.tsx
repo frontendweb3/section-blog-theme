ed453c88f7b996c06479179525cbae96129848f0
Rajdeep Singh <radewala.2@gmail.com>
2023-11-15 12:39:14 +0530

    remove the padding and adding container

--- packages/section-blog-theme/components/Article/Article.tsx
+++ packages/section-blog-theme/components/Article/Article.tsx
@@ -8,6 +8,6 @@ interface ArticleProps {
 
 export function Article({ className, children }: ArticleProps) {
   return (
-    <article className={cn("py-3 px-3 mt-5 mb-20 sm:px-0 prose dark:prose-p:text-white prose-img:mx-auto prose-pre:bg-slate-700/25 dark:prose-pre:bg-primary-foreground prose-zinc sm:prose-sm md:prose-base lg:prose-lg xl:prose-xl 2xl:prose-2xl dark:prose-invert mx-auto", className)}> {children} </article>
+    <article className={cn("container mt-10 mb-20 sm:px-0 prose dark:prose-p:text-white prose-img:mx-auto prose-pre:bg-slate-700/25 dark:prose-pre:bg-primary-foreground prose-zinc sm:prose-sm md:prose-base lg:prose-lg xl:prose-xl 2xl:prose-2xl dark:prose-invert mx-auto", className)}> {children} </article>
   );
 }
