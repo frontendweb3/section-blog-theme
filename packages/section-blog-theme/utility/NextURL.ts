8cab0e8bc55715f6167c8ad7b14c2fcb8b941b20
Rajdeep Singh <radewala.2@gmail.com>
2023-11-15 12:24:16 +0530

    fix the tags issue

--- packages/section-blog-theme/utility/NextURL.ts
+++ packages/section-blog-theme/utility/NextURL.ts
@@ -1,17 +1,19 @@
 
 export function Next_URL(Site: string | undefined): string {
 
-  let getPort = process.env.PORT || 3000
-
-  let getDomain = `http://localhost:${getPort}`
-
-  if (process.env.NODE_ENV === 'development' || getPort) {
-    return getDomain + "/"
-  } else {
-    if (Site) {
-      return Site
-    } else {
-      return getDomain + "/"
-    }
+  if (process.env.NODE_ENV === 'development') {
+    return "/"
   }
+
+  if (process.env.NODE_ENV === 'production' && Site) {
+
+    return Site
+
+  } else {
+
+    throw new Error("Please add your production URL in theme.config.tsx file.")
+
+  }
+
+
 }
