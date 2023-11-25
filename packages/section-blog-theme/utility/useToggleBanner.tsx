25bab1c16244d499df532021721299ab29bf0eac
Rajdeep Singh <radewala.2@gmail.com>
2023-11-25 12:51:30 +0530

    remove the unused code

--- packages/section-blog-theme/utility/useToggleBanner.tsx
+++
@@ -1,9 +0,0 @@
-import { useLocalStorage } from 'usehooks-ts'
-
-export const useBannerCookies = () => {
-  const [banner, setBanner] = useCookie('banner', "show");
-  const hideBanner = () => {
-    setBanner("hide");
-  };
-  return { banner, hideBanner };
-};
