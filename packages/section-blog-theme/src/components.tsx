bea77c900be1dc038145c379ae9f26e1f45cb97a
Rajdeep Singh <radewala.2@gmail.com>
2023-12-29 21:16:08 +0530

    adding comment

--- packages/section-blog-theme/src/components.tsx
+++ packages/section-blog-theme/src/components.tsx
@@ -1,4 +1,4 @@
-export { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"; // tab component
-export { Separator } from "@/components/ui/separator"; // separator component
-export { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"; // alert component
-export { Space } from "@/components/Space/Space"; // space component
+export { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"; // tab component from shadcn ui
+export { Separator } from "@/components/ui/separator"; // separator component from shadcn ui
+export { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"; // alert component from shadcn ui
+export { Space } from "@/components/Space/Space"; // space component from shadcn ui
