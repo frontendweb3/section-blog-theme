3280be443e148f8830c3613dee0f4cc91b2793f3
Rajdeep Singh <radewala.2@gmail.com>
2023-12-29 21:13:32 +0530

    update UI

--- examples/demo-section-blog/src/components/Newsletter.tsx
+++ examples/demo-section-blog/src/components/Newsletter.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export function Newsletter() {
   return (
-    <section className="!bg-blue-400 my-16 py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+    <section className="bg-blue-400 my-16 py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
       
       <div className="mx-auto max-w-screen-md sm:text-center">
         
@@ -10,7 +10,7 @@ export function Newsletter() {
           Sign up for Our Newsletter
         </h2>
         
-        <p className="mx-auto mb-8 max-w-2xl font-light  md:mb-12 sm:text-xl !text-black">
+        <p className="mx-auto mb-8 max-w-2xl font-light  md:mb-12 sm:text-xl text-black">
           Stay up to date with the roadmap progress, announcements and exclusive
           discounts feel free to sign up with your email.
         </p>
@@ -24,7 +24,7 @@ export function Newsletter() {
               Email address
             </label>
             <input
-              className="block p-3 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:rounded-none sm:rounded-l-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+              className="block p-3 pl-10 w-full text-sm text-white bg-gray-50 rounded-lg border border-gray-300 sm:rounded-none sm:rounded-l-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:focus:ring-primary-500 dark:focus:border-primary-500"
               placeholder="Enter your email"
               type="email"
               id="email"
@@ -33,13 +33,13 @@ export function Newsletter() {
           <div className="mx-2">
             <button
               type="submit"
-              className="py-3 px-5 w-full text-sm font-medium text-center text-white dark:!text-black rounded-lg border cursor-pointer bg-primary-700 border-primary-600 sm:rounded-none sm:rounded-r-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+              className="py-3 px-5 w-full text-sm font-medium text-center rounded-lg border border-primary-600 sm:rounded-none sm:rounded-r-lg focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-800"
             >
               Subscribe
             </button>
           </div>
         </div>
-        <div className="mt-3 mx-auto max-w-screen-sm text-sm text-center text-gray-500 newsletter-form-footer dark:!text-black">
+        <div className="mt-3 mx-auto max-w-screen-sm text-sm text-center text-gray-00 newsletter-form-footer dark:text-black">
           We care about the protection of your data.{" "}
           <a
             href="#"
