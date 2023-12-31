72c5cf93991387fc055368f8a614219bd5a6b43c
Rajdeep Singh <radewala.2@gmail.com>
2023-12-31 19:10:45 +0530

    fix the margin issue

--- packages/section-blog-theme/components/Article/Article.tsx
+++ packages/section-blog-theme/components/Article/Article.tsx
@@ -8,6 +8,6 @@ interface ArticleProps {
 
 export function Article({ className, children }: ArticleProps) {
   return (
-    <article className={cn("px-5 sm:px-0 mt-12 mb-20 prose dark:prose-p:text-white prose-img:mx-auto prose-pre:bg-primary-foreground prose-slate sm:prose-sm md:prose-base lg:prose-lg xl:prose-xl 2xl:prose-1xl dark:prose-invert mx-auto", className)}> {children} </article>
+    <article className={cn("mt-5 px-5 sm:px-0 mb-20 prose dark:prose-p:text-white prose-img:mx-auto prose-pre:bg-primary-foreground prose-slate sm:prose-sm md:prose-base lg:prose-lg xl:prose-xl 2xl:prose-1xl dark:prose-invert mx-auto", className)}> {children} </article>
   );
 }
