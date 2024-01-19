0385322491f9d8fa579a9d97e0109dcd53c9a6f5
Rajdeep Singh <radewala.2@gmail.com>
2024-01-19 16:31:40 +0530

    remove the unused css and fix the css

--- packages/section-blog-theme/components/Article/Article.tsx
+++ packages/section-blog-theme/components/Article/Article.tsx
@@ -10,7 +10,7 @@ export function Article({ className, children }: ArticleProps) {
   return (
     <article
       className={cn(
-        "max-w-none nx-mt-5 nx-px-5 sm:nx-px-0 nx-mb-20 nx-format nx-format-section nx-format-img:nx-mx-auto sm:nx-format-sm md:nx-format-base lg:nx-format-lg xl:nx-format-2xl 2xl:nx-format-2xl nx-mx-auto",
+        "nx-mt-5 nx-px-5 sm:nx-px-0 nx-mb-20 nx-format nx-format-section nx-format-img:nx-mx-auto sm:nx-format-sm md:nx-format-base lg:nx-format-lg nx-mx-auto",
         className,
       )}
     >
