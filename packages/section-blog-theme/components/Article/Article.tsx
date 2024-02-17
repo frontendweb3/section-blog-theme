7d1b7ede7e1d55bf9389ad897c94cfce8a9999da
Rajdeep Singh <radewala.2@gmail.com>
2024-02-17 22:17:53 +0530

    adding padding around in article

--- packages/section-blog-theme/components/Article/Article.tsx
+++ packages/section-blog-theme/components/Article/Article.tsx
@@ -10,7 +10,7 @@ export function Article({ className, children }: ArticleProps) {
   return (
     <article
       className={cn(
-        "nx-max-w-4xl nx-mt-5 nx-px-5 sm:nx-px-0 nx-mb-20 nx-format nx-format-section nx-format-img:nx-mx-auto sm:nx-format-sm md:nx-format-base lg:nx-format-lg nx-mx-auto",
+        "nx-max-w-4xl nx-px-4 nx-py-4 nx-mb-20 nx-format nx-format-section nx-format-img:nx-mx-auto sm:nx-format-sm md:nx-format-base lg:nx-format-lg nx-mx-auto",
         className,
       )}
     >
