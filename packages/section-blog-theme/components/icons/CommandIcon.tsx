e0e6c08ac400a8d8626f9dee5ca164fe6165dad7
Rajdeep <radewala.2@gmail.com>
2024-04-14 14:43:12 +0530

    creating icons

---
+++ packages/section-blog-theme/components/icons/CommandIcon.tsx
@@ -0,0 +1,5 @@
+export function CommandIcon() {
+  return (
+    <svg className="nx-h-5 nx-w-5" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3"/></svg>
+  )
+}
---
+++ packages/section-blog-theme/components/icons/icons.ts
@@ -0,0 +1,3 @@
+export { FileIcon } from "./FileIcon";
+export { CommandIcon } from "./CommandIcon";
+
